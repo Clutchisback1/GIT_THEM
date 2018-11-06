8ab69f89b88783503337fb9ca067d484761146dd
Clutchisback1 <nathaniel.curry1@gmail.com>
2018-11-05 21:12:25 -0700

    Update and rename README.md to getem.sh

---
+++ getem.sh
@@ -0,0 +1,97 @@
+#Downloads just about all of the github goodies I like...
+
+echo 'GET THEM!!!!'
+cat << "EOF"
+                                                                `.-/+oosssssssso:`
+                                                          `-/+osyysssssyyyyyysoosyy+.
+                                                     `:+ssssssyyhhhhhhhhhhhhhhhhhyssyy+.            
+                                                   -ssoosyhhhhhhyyyyyyyyyyyyyyyyhhhhhysyy+.         
+                                                 .osoyhhhyyyyyyyyyyyyyyyyyyyyyyyyyyyhhhysshs.       
+                                                /yoyhhyyyhdhyyyyyyyyyyyyyyyyyyyyyyyyyyyhhyooh+      
+                                              `ssyhhyyyhhysssyyyyyyyyyyyyyyyyyyyyyyyyyyyyhds/ys     
+                                             -yyhhyyyhyooyhhhdyyyyyyyyyyyyyyyyyyyyyyyyyyyyhhs:d/    
+                                            /yyhyyyhdsoyys/--ddhyyyyyyyshhyyyyyyyyyyyyyyyyyhh+oh    
+                                         :+sdyyyyyydsyh/.:+  dhmyyyyyyyssNyyyyyysdhyyyyyyyyyhy/N`   
+                                         yydhyyyyyyyhs.  hm/`dhmdyyyyyys+Nhyyyyysddyyyyyyyyyyh+d+   
+                                         -shmydshyyyy`   sNh/:-/myyyyyyy/hhyyyyhoddyyyyyshyyyhsyy   
+                                   `.     `-yshssysm:--.-oy-..``yhyyyyhh/ydyyyyhodmyyyyh+hdyyhyod   
+                                 `/+o+  `..-:syo/:::---..:``-:o+/dyyyyhh:ymyyyyhsdmyyyyh+hmyyhhom.  
+                         `.---::+o:-`h`+dhymd.````````````:/-.-::myyyyhh:ymyyyyhodmyyyyh+hNhyhdom-  
+                     .-/+/:----h/-`  +s:/oyh/....```````.::-/+osyNyyyyhh.hmyyyhh+dmyyyyh+hNhyhdsd-  
+                  `-//:.`     .o`    /o:++--//::-....-:/+sssyyyysmyyyyhy.mmyyyhh/hmyyyyh/dNhyhdod-  
+                 :o:.``       `o/-..:+`  .ys+o+:ysssossyyyyhhyhhymhyyhho:Ndyyyhh:hmhyyhy:dmhyhdod-  
+               .o+..`           -///.     .d/:ohmoossyhhyyhhddmhymhyhhd/oNdyyyhy-hmhyyhy-dmhyhh+m`  
+          `..:/sy+/`             ``       `h:``ddyhhyyhhdddhdhyyymhyhhh:mmhyyhhy.mmhyyhs:NNhhhyyy   
+        -+:/-.    -+`                    ``h` -mmhyhhyyhs++shyyyymhhhdsoNmhyyhho-Nmhyyho:Nydhdom-   
+        -o+:--.``  +-                   `.+o-/hdhdyo:. `.+yyyyyyhmhhhh/mmdhyhhd-oNdyyyh/+h.dyysh    
+          .:+o/:::+/            .--```.-:smdhhdmmmy:-:+shyyyyyyhhNhhdosMmdyyhhy.mmdyyhh-so +s/m-    
+             ssshs:           .sho/://oyddhhhsddhyyhhhyyyyyyyhhyymyds/hdmhyhhh:oNdhyyhy.d- `dds     
+             /+`oo.`      ```-dmhshdddhhhyhdydmyyyyyyyyyyyyyhhosNyhy:y:Nmhyhdo:NNdyyhh+/d   sm`     
+              o/`.--.......:sy+++odhyyhho-dmyNoyyyyyyyyyyyyhs+hhs+y-s/-Ndhhhy:hsNdyhhh:y+   ``      
+               :ss/----:+sys:--.  `odds./yyshdohyyyyyyyyyhy+smo`o--s+ omhhhh/y++Nhyhhs/d`           
+              `.oNNNmdhyo/:-.`     `oMy+:ssymyshyyyyyyyyhs:yNm-`hoh/ `dhhhh/ss ymyhhh/y+            
+            -os+:.`````.-/+oo-`````..M+ sysydoyhyyyyyyyyh/oNmhdyy+`  :dhhh/ss  hdhhdo/d`            
+          `os.              .+yo.`.`+h` ::. +sshhdddhyyyhs/NNdyoys.  ysdh:s+  .mhhhy/h:             
+         `y/                   -s+`-m.      .d:hyydoodhyyhoomhhhyosoooyy:y/-+smdhdh:yh
+         so                     `:dM+        y/shydo `hdyyhosdyyhhhsd-+/mdyysymshh-smds             
+        :d                      ```sy`       .h:hyym` `hdyyhssdyyyhds:smdsyhyydoh-smhsss            
+        d:                       `` /m.       y/shyhy  `hhyyhyodhyymhddmdyyhdNo/-yydhhoy/           
+       .m`                        `` +d`      -h:hyym/  `yhyyhh+dmdhhyyhyhdy:d:-h/ .yhh+d-          
+       +s                         ````Nh+.     h/shyhm`  `hhyyhh+yNshdddy/` -m+d-    /dh/d`         
+       y/`                        ``` m+od+    /h/hyyhy   `hhyyhhood-.-`    +my.      `o+so         
+      -N/`                        ``.`h`..m-    m+shyym/   `hhyyhho/d:      .:          -sm`        
+     .dh+``                       `.`:s`.`so    oh/hhyhm:`` .hhyyhhs:do                   -`        
+    .y--h..`                    ``  `ys`.`h/  `-oN+shyydNmdyo-dhyyhhs:yy`
+    y-.`+o..```       ./++++++- ` -so.y/.-m. omshmmhmdddddhym/.dhyyhhy:sd:-..`
+   ++.`  os::+/:```` -o-.    `o/  s:``+/.h+  doohhhhhhyyssssyo -Nhyyyhh:+mmdddy/
+  `h`.-:/+oooo+sy...`s--`     `h`````..-ho   yo+hyyysssssssssy:ommhyyyhh+hNddsyN.
+  .y+/-.`   ``.--++-.y--`     :h/..``-oy:   .sd+syyyyyssssssddhhdmNmddmmdhysoo+mo
+                  -osm+..   `+mhysosso:`.-+/+oyhhyosyyhhyyydNsyhhhhhhhyssyyyyy:od
+                    `oy`-`.+y+----.`-/oh+/oysyyyoosymdNd/::dNhyyyyyyssssssssyho:N.
+                     .m.:sy+``-/+syhdyosysosh/++hddddh+:--yhdmssssssssssssssyhy-m+
+                      +ys:`:sddhhhhhhhhyyys+oohhyyhs/-----dhhmmsssssssssssyyyyooNmy:
+                       ` .sdhhyyyyyyyyyyyyhhhymms+:----:::hdhhNdyssssssssssssymdhhhdo`
+                        `hdhyhhhhhhhhhyyhhhhhhdNo---:::///ohsyNNs+ohhhhhdd+osyyddhhhds
+                        omhhhhhyhhhhhhhhhhhhhhhhm::////::/hmmNmo/+yhs++yhyooyhh/:yddyN-             
+                       :mdyhhhhhhhhhhhhhhhhhhhhdm//:::/sdmdmNh+/+yy/-/oho/+sdsy---omyd+             
+                       mhhyyyyhhhhhhhhhhhdddddhhm:/+ssyyyydsyhoshh-``hh///sy+d:--::oddo             
+                       ymysyysooosssssssyysssssydoyysy+:-dyssssoo+++/yhssyh+ho---://sMo             
+                       `+hhsyssyssoo++++oossyssssyyo:-/yhhhhhhhhhhyyso++ss+od---::///Mo             
+                         `:osyyooososoosssso+oss+:``+hhyhyhhhhhyyyyhhhdhy+oNs--:://-sM+             
+                             .:/oooooo++oooo+/.   .yhhyyyyyyyyyyyyyyyyyhhdmmds:://-:NN:             
+                                   ``....`        hhyyyyhhhhhhhhhyyhhhhhhhhhhdh//:-dmm`             
+                                                 -mhhhhhhhhhhhhhhhhhhhhhhhhhhdmo-.sydy
+                                                `ymyhhhhhhhhhhhhhhhhhhhhhdddddhh-sshm.
+                                                oNdhyyhhhhhhhhhhhddddddddddhsoymsshm+
+                                                ddyshhyssssyyhhhdddddhhyso++ohhsssd+
+                                                /dhhssssyssoo++/////:://osyyyoo+sh:
+                                                 .ohsoooosssssyyssyysyyyys+///oho`
+                                                   `:oyyo/://++////::::-..:ohy/`
+                                                       `-/ossssso++++ossyyo:`
+EOF
+
+git clone https://github.com/D35m0nd142/LFISuite.git
+git clone https://github.com/linted/linuxprivchecker.git
+git clone https://github.com/m4ll0k/WAScan.git
+git clone https://github.com/archerysec/archerysec.git
+git clone https://github.com/maurosoria/dirsearch.git
+git clone https://github.com/codingo/Reconnoitre.git
+git clone https://github.com/RenwaX23/XSS-Payloads.git
+git clone https://github.com/caffix/amass.git
+git clone https://github.com/cyberheartmi9/PayloadsAllTheThings.git
+git clone https://github.com/TheSecondSun/Bashark.git
+git clone https://github.com/s0md3v/AwesomeXSS.git
+git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git
+git clone https://github.com/s0md3v/Photon.git
+git clone https://github.com/LostRabbitLabs/AnomalousCookie.git
+git clone https://github.com/stealth/sshttp.git
+git clone https://github.com/mps-youtube/mps-youtube.git
+git clone https://github.com/joaomatosf/jexboss.git
+git clone https://github.com/zerosum0x0/koadic.git
+git clone https://github.com/D4Vinci/Cr3dOv3r.git
+git clone https://github.com/1N3/Sn1per.git
+git clone https://github.com/SpiderLabs/Responder.git
+git clone https://github.com/byt3bl33d3r/CrackMapExec.git
+git clone https://github.com/rebootuser/LinEnum.git
+
+echo 'start Pwning...'
